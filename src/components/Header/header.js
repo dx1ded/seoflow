7467c1c70a41e98781c4cfe844092132c8d51ac6
Volodymyr Doskochinskiy <doskovol1@gmail.com>
2022-06-01 23:01:19 +0300

    Added stucked header top

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,12 +2,14 @@ const headerTop = document.querySelector(".header__top")
 const stickyElement = document.querySelector("#header-sticky")
 
 const headerTopHeight = headerTop.scrollHeight
+const stickyElementTopPosition = 20
+
 const stickyElementActiveClassName = "header__bottom--active"
 
 function scrollHandler() {
   const yAxis = window.scrollY
 
-  if (yAxis >= headerTopHeight) {
+  if (yAxis >= headerTopHeight - stickyElementTopPosition) {
     return stickyElement.classList.add(stickyElementActiveClassName)
   }
 
